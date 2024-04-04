21ee94ba3b0206c2ee08e63dcac5d77327af69de
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-04-04 12:50:35 +0300

    +

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    """Parses a string of email addresses into an array of unique, sorted email addresses."""
+
+    def __init__(self, input_email_addresses):
+        """Initializes the parser with a string of email addresses."""
+        self.input_email_addresses = input_email_addresses
+
+    def parse(self):
+        """Parses the input string into an array of unique, sorted email addresses."""
+
+        # Split the input string into individual email addresses using a regular expression.
+        parsed_email_addresses = re.split(r"[ ,]", self.input_email_addresses)
+
+        # Convert the list to a sorted set to remove duplicates and sort the addresses alphabetically.
+        parsed_email_addresses = sorted(set(parsed_email_addresses))
+
+        return parsed_email_addresses
