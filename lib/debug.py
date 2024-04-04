c2029b4f8945dd97d44cedaa1459fd5b6db2bc43
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-04-04 12:52:39 +0300

    *

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python3
+import pytest
 
-from email_address_parser import EmailAddressParser
+def test_parse_csv():
+    email_addresses = "john@doe.com, person@somewhere.org"
+    parser = EmailAddressParser(email_addresses)
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    assert parser.parse() == ["john@doe.com", "person@somewhere.org"]
+
+def test_parse_space():
+    email_addresses = "john@doe.com person@somewhere.org"
+    parser = EmailAddressParser(email_addresses)
+
+    assert parser.parse() == ["john@doe.com", "person@somewhere.org"]
+
+def test_parse_unique():
+    email_addresses = "john@doe.com, john@doe.com, person@somewhere.org"
+    parser = EmailAddressParser(email_addresses)
+
+    assert parser.parse() == ["john@doe.com", "person@somewhere.org"]
+
+def test_parse_sorted():
+    email_addresses = "person@somewhere.org, john@doe.com"
+    parser = EmailAddressParser(email_addresses)
+
+    assert parser.parse() == ["john@doe.com", "person@somewhere.org"]
